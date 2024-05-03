52a2435c92aa9fd6de1ae3a934a7d15bad0bed53
arturbogea <arturbogea@gmail.com>
2024-05-03 07:56:50 -0300

    Criando a tela de detalhes dos filmes, onde vão receber imagens e informações de uma API.

--- app/src/main/java/com/arturbogea/homecinema/adapter/AdapterFilme.kt
+++ app/src/main/java/com/arturbogea/homecinema/adapter/AdapterFilme.kt
@@ -1,6 +1,7 @@
 package com.arturbogea.homecinema.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.arturbogea.homecinema.databinding.FilmeItemBinding
 import com.arturbogea.homecinema.model.Filme
+import com.arturbogea.homecinema.view.DetalhesFilme
 import com.bumptech.glide.Glide
 
 class AdapterFilme(private val context: Context, private val listaFilmes: MutableList<Filme>):
@@ -24,6 +26,11 @@ class AdapterFilme(private val context: Context, private val listaFilmes: Mutabl
 
     override fun onBindViewHolder(holder: FilmeViewHolder, position: Int) {
         Glide.with(context).load(listaFilmes[position].capa).centerCrop().into(holder.capa)
+
+        holder.capa.setOnClickListener {
+            val intent = Intent(context,DetalhesFilme::class.java)
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
